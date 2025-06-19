62979f73d337b1d06b29b19e5d7851393f872e3a
Yuyi <roshanisingh2354@gmail.com>
2025-06-19 17:37:48 +0530

    INIT Project setup

---
+++ src/pages/CreatePage.jsx
@@ -0,0 +1,9 @@
+function CreatePage() {
+  return (
+    <>
+    <h1>Create Page</h1>
+    </>
+  )
+}
+
+export default CreatePage;
---
+++ src/pages/NoteDetailPage.jsx
@@ -0,0 +1,9 @@
+function NoteDetailPage() {
+  return (
+    <>
+    <h1>Note</h1>
+    </>
+  )
+}
+
+export default NoteDetailPage;
